15a7f47ac319f974424ff9d7d6037f3fc88a1971
tejaswi-Boni <tejaswirajmohan@gmail.com>
2024-07-14 17:24:39 +0800

    code modified

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from setuptools import find_packages, setup
 setup(
     name="financebot",
     version="0.0.1",
-    author="sunny",
-    author_email="sunny.savita@ineuron.ai",
+    author="Teja",
+    author_email="tejaswi1314@gmail.com",
     packages =find_packages(),
     install_requires=["langchain","langchain-openai","langchain-astradb","datasets","pypdf","python-dotenv","flask"]
 )
